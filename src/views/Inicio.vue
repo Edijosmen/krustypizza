c422d84c17fe09866d1e9d6fafa7a0bb991324d1
edinson <josment123jf@gmail.com>
2020-10-08 17:25:34 -0500

    pginicio

---
+++ src/views/Inicio.vue
@@ -0,0 +1,142 @@
+<template>
+  <section class = "row justify-content-center">
+      <div class = "col-md-6">
+           <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="../assets/img/fondo2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <img src="../assets/img/fondo2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <img src="../assets/img/fondo2.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+      </div>
+  </section>
+  <section>
+        <div>
+            <img src="images/krostyanuncio.jpg" width="120"> Krusty Pizzería
+        </div>
+    <div class="container">
+      <nav class="navbar navbar-light navbar-expand-sm" style="background-color: #f4f9fc;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+         <div class="navbar-nav mx-auto text-center">
+          <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Pizzas</a>
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Bebidas</a>
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Postres</a>
+         </div>
+        </div>
+      </nav>
+    </div>
+  </section>
+  <section class="container">
+      <div class="row">
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+          <div class="col-sm-12 col-md-4">
+              <div>
+                  <div class="card" style="width: 18rem;">
+                        <img src="../assets/img/fondo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+              </div>
+          </div>
+      </div>
+  </section>
+</template>
